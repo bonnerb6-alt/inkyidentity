33f727f0c1f194ad125d5ae7e7d770bbc81a793a
Brian Bonner <bonnerb6@gmail.com>
2026-04-16 09:20:01 +0100

    update

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 import { cookies } from 'next/headers';
 
-const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_SECRET = process.env.JWT_SECRET!;
 if (!JWT_SECRET) throw new Error('JWT_SECRET environment variable is not set');
 const COOKIE_NAME = 'ii_token';
 
